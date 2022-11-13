921cd6a5bcede5034058229e19f50d81e2288223
Prithviraj <prithviraj792000@gmail.com>
2022-11-13 10:34:31 -0500

    installed rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+#![allow(unused)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
