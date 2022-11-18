fd25b7b973b7e85e1120274cef0cb78b1d92d3ad
ubdussamad <ubdussamad@gmail.com>
2022-11-18 18:42:43 -0500

    Adding filtering to mnemonic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod mnemonic_generator;
 
 fn main() {
-    println!("{}", mnemonic_generator::generate_random_string());
+    println!("{}", mnemonic_generator::generate_random_string().unwrap_or_default());
 }
--- src/mnemonic_generator.rs
+++ src/mnemonic_generator.rs
@@ -1,8 +1,12 @@
 use rand::distributions::{Alphanumeric, DistString};
 
-pub fn generate_random_string() -> String {
+pub fn generate_random_string() -> Option<String> {
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
     // Implement logic to make it sound like a mnemonic.
     // Also check if the new string is already in the database.
-    string
+    if string.contains('@') {
+        return None;
+    }
+
+    Some(string)
 }
