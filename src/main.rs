0245e506404b806d00c1cdaf3ba99727d2b35bf1
ubdussamad <ubdussamad@gmail.com>
2022-11-19 00:44:04 -0500

    Setup basic server,fancy stuff reamins.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,57 @@
-mod mnemonic_generator;
+// mod mnemonic_generator;
 
-fn main() {
-    println!("{}", mnemonic_generator::generate_random_string().unwrap_or_default());
+// fn main() {
+//     // println!("{}", mnemonic_generator::generate_random_string().unwrap_or(String::from("Err")));
+//     let string : Option<String> = mnemonic_generator::generate_random_string();
+//     match string {
+//         Some(s) => {
+//             println!("{}", s)
+//         },
+//         None => {
+//             println!("Err: String contains u");
+//         }
+//     }
+
+// }
+
+#[macro_use]
+extern crate rocket;
+use dashmap::DashMap;
+use rand::Rng;
+// use rocket::State;
+
+#[get("/index.html")]
+fn index() -> &'static str {
+    "<html> <h1 style=\"text-align: center;\">Welcome to the Mnemonic Generator!</h1> <p style=\"text-align: center;\">This is a simple web app that generates a random mnemonic.</p> <p style=\"text-align: center;\">To generate a mnemonic, go to <a href=\"/generate\">/generate</a>.</p> </html>"
+}
+
+#[get("/favicon.ico")]
+fn img() -> &'static str {
+    "Lochya!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(DashMap::<u32, String>::new())
+        .mount("/", routes![index, img, shorten, redirect])
+}
+
+#[post("/short?<url>")]
+fn shorten(url: String, state: &rocket::State<DashMap<u32, String>>) -> Result<String, rocket::response::status::BadRequest<&str>> {
+    if url.is_empty() {
+        Err(rocket::response::status::BadRequest(Some("URL is empty!")))
+    } else {
+        let key: u32 = rand::thread_rng().gen();
+        state.insert(key, url);
+        Ok(key.to_string())
+    }
+}
+
+#[get("/<key>")]
+fn redirect(key: u32, state: &rocket::State<DashMap<u32, String>>) -> Result<rocket::response::Redirect, rocket::response::status::NotFound<&str>> {
+    state
+        .get(&key)
+        .map(|url| rocket::response::Redirect::to(url.clone()))
+        .ok_or(rocket::response::status::NotFound("Invalid or expired link!"))
 }
--- src/mnemonic_generator.rs
+++ src/mnemonic_generator.rs
@@ -4,7 +4,7 @@ pub fn generate_random_string() -> Option<String> {
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
     // Implement logic to make it sound like a mnemonic.
     // Also check if the new string is already in the database.
-    if string.contains('@') {
+    if string.contains('u') {
         return None;
     }
 
