46f5533f38276450fa003ccdca9723bac0776939
ubdussamad <ubdussamad@gmail.com>
2022-11-22 21:26:00 -0500

    Added the front page with poor looking graphics

--- src/main.rs
+++ src/main.rs
@@ -33,18 +33,27 @@ fn rocket() -> _ {
         )
 }
 
-#[get("/shorten?<url>")]
-fn shorten(url: String, state: &rocket::State<DashMap<u32, String>>) -> Result<String, rocket::response::status::BadRequest<&str>> {
-
+#[get("/shorten?<url>&<translation_type>")]
+fn shorten(url: String, translation_type : String, state: &rocket::State<DashMap<u32, String>>) -> Result<String, rocket::response::status::BadRequest<&str>> {
+    // TODO: Three translation types:
+    // 1. Random number (default)
+    // 2. Memonic TODO
+    // 3. Emoji   TODO
     println!( "URL: {}", url);
     if url.is_empty() {
         Err(rocket::response::status::BadRequest(Some("URL is empty!")))
     } else {
+        // Impliment type trigred url key change here.
         let key: u32 = rand::thread_rng().gen();
         state.insert(key, url);
+        // print out the key to the rocket console
+        println!("Key: {}", translation_type);
         Ok(key.to_string())
     }
 }
+
+
+// Add Api for login and register, so that user can add a custom url
 
 #[get("/<key>")]
 fn redirect(key: u32, state: &rocket::State<DashMap<u32, String>>) -> Result<rocket::response::Redirect, rocket::response::status::NotFound<&str>> {
